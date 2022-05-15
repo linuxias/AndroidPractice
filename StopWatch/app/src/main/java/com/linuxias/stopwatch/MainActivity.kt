05dca36855937fa4f6c2b2940912b5b14ec630c1
Seungha Son <seungha.son@samsung.com>
2022-05-15 14:53:34 +0900

    Add button click event to MainActivity
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

--- StopWatch/app/src/main/java/com/linuxias/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/linuxias/stopwatch/MainActivity.kt
@@ -2,10 +2,45 @@ package com.linuxias.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.linuxias.stopwatch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var isRunning = false
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener(this)
+        binding.btnRefresh.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_start -> {
+                if (isRunning)
+                    pause()
+                else
+                    start()
+            }
+            R.id.btn_refresh -> {
+                refresh()
+            }
+        }
+    }
+
+    private fun start() {
+
+    }
+
+    private fun pause() {
+
+    }
+
+    private fun refresh() {
+
     }
 }
