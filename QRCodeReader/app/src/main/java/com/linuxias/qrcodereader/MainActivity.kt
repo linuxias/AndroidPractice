8c63d60b428d85fc535a4ade8949d0e0b5de0fb1
Seungha Son <seungha.son@samsung.com>
2022-05-22 02:09:53 +0900

    QRCodeReader: Init project and layout
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

---
+++ QRCodeReader/app/src/main/java/com/linuxias/qrcodereader/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.linuxias.qrcodereader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.linuxias.qrcodereader.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
