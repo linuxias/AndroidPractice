1696bad8df777b292df4a323cc61f0a078e4e5e1
Seungha Son <seungha.son@samsung.com>
2022-05-27 16:29:25 +0900

    QRCodeReader: Implement MainActivity
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

--- QRCodeReader/app/src/main/java/com/linuxias/qrcodereader/MainActivity.kt
+++ QRCodeReader/app/src/main/java/com/linuxias/qrcodereader/MainActivity.kt
@@ -1,15 +1,63 @@
 package com.linuxias.qrcodereader
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.linuxias.qrcodereader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture:
+            ListenableFuture<ProcessCameraProvider>
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { result ->
+        if (result.all { it.value }) {
+            startCamera()
+        } else {
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        permissionLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun getPreview() : Preview {
+        val preview : Preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.barcordPreview.getSurfaceProvider())
+
+        return preview
+    }
+
+    private companion object {
+        val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA
+        )
     }
 }
