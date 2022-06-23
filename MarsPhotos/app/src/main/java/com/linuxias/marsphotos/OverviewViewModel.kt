8db85e5180495773763322179678f85d40e3e698
Seungha Son <seungha.son@samsung.com>
2022-06-23 23:04:36 +0900

    MarsPhotos: Connect service using retrofit
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

---
+++ MarsPhotos/app/src/main/java/com/linuxias/marsphotos/OverviewViewModel.kt
@@ -0,0 +1,37 @@
+package com.linuxias.marsphotos
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.linuxias.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<String> = _status
+    /**
+     * Call getMarsPhotos() on init so we can display status immediately.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    /**
+     * Gets Mars photos information from the Mars API Retrofit service and updates the
+     * [MarsPhoto] [List] [LiveData].
+     */
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            val listResult = MarsApi.retrofitService.getPhotos()
+            _status.value = listResult
+        }
+    }
+}
---
+++ MarsPhotos/app/src/main/java/com/linuxias/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,23 @@
+package com.linuxias.marsphotos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .build()
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
+
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): String
+}
