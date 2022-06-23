785fc81a7bd5bf79e2e3c58beb6bb0eeb44ab271
Seungha Son <seungha.son@samsung.com>
2022-06-24 00:33:43 +0900

    MarsPhotos: Complete this project
     https://developer.android.com/codelabs/basic-android-kotlin-training-getting-data-internet
    
    Signed-off-by: Seungha Son <seungha.son@samsung.com>

--- MarsPhotos/app/src/main/java/com/linuxias/marsphotos/network/MarsApiService.kt
+++ MarsPhotos/app/src/main/java/com/linuxias/marsphotos/network/MarsApiService.kt
@@ -1,14 +1,21 @@
 package com.linuxias.marsphotos.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build()
 
 object MarsApi {
@@ -19,5 +26,5 @@ object MarsApi {
 
 interface MarsApiService {
     @GET("photos")
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
---
+++ MarsPhotos/app/src/main/java/com/linuxias/marsphotos/network/MarsPhoto.kt
@@ -0,0 +1,10 @@
+package com.linuxias.marsphotos.network
+
+import com.squareup.moshi.Json
+
+data class MarsPhoto(
+    val id: String,
+
+    @Json(name = "img_src")
+    val imgSrcUrl: String
+)
